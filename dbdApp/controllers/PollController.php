2b9643be588cd03c86ad36f52816fc73c7c657df
Will Mason <get@willmason.me>
2013-01-23 01:37:01 -0800

    change the format to be like notifyr

--- dbdApp/controllers/PollController.php
+++ dbdApp/controllers/PollController.php
@@ -3,7 +3,7 @@ class PollController extends HYController {
 
 	public function doDefault() {
 		if ($power = Power::getNext()) {
-			echo $power->getStatus() ? 'on' : 'off';
+			echo 'data: "' . ($power->getStatus() ? 'on' : 'off') . '"';
 			$power->setRead(1);
 			$power->save();
 		}
