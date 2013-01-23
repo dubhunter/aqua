22c96fbb21185e103339d9ebc87abdcc04e59577
Will Mason <get@willmason.me>
2013-01-23 01:39:19 -0800

    add some newlines

--- dbdApp/controllers/PollController.php
+++ dbdApp/controllers/PollController.php
@@ -3,7 +3,7 @@ class PollController extends HYController {
 
 	public function doDefault() {
 		if ($power = Power::getNext()) {
-			echo 'data: "' . ($power->getStatus() ? 'on' : 'off') . '"';
+			echo 'data: "' . ($power->getStatus() ? 'on' : 'off') . '"' . PHP_EOL . PHP_EOL;
 			$power->setRead(1);
 			$power->save();
 		}
