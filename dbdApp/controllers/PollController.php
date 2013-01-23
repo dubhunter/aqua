71f7ba443de1b0cb2c3a1bdbd9eee86e4a8dc329
Will Mason <get@willmason.me>
2013-01-23 01:52:39 -0800

    remove some newlines

--- dbdApp/controllers/PollController.php
+++ dbdApp/controllers/PollController.php
@@ -3,7 +3,7 @@ class PollController extends HYController {
 
 	public function doDefault() {
 		if ($power = Power::getNext()) {
-			echo PHP_EOL . PHP_EOL . 'data: "' . ($power->getStatus() ? 'on' : 'off') . '"' . PHP_EOL . PHP_EOL;
+			echo 'data: "' . ($power->getStatus() ? 'on' : 'off') . '"' . PHP_EOL . PHP_EOL;
 			$power->setRead(1);
 			$power->save();
 		}
