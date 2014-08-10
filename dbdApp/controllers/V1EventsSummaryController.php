1c5b267a52a66588c0bd5e58c2df32e4bc632874
dubhunter <get@willmason.me>
2014-08-10 00:51:27 -0700

    check the if

--- dbdApp/controllers/V1EventsSummaryController.php
+++ dbdApp/controllers/V1EventsSummaryController.php
@@ -38,6 +38,7 @@ class V1EventsSummaryController extends V1ApiController {
 			$sum_count++;
 			dbdLog($sum);
 			dbdLog($sum_count);
+			dbdLog("$date > ($start + ($downsample * $step_count) + $downsample) || $i = 0");
 			if ($date > ($start + ($downsample * $step_count) + $downsample) || $i = 0) {
 				$data[] = array(
 					'data' => round($sum / $sum_count),
